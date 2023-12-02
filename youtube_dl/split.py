8f2ae1a04885a681caf16fb3c972618ff06efa41
kphantha <kphantha@uoguelph.ca>
2023-12-01 23:20:11 -0500

    split fix

--- youtube_dl/split.py
+++ youtube_dl/split.py
@@ -11,21 +11,24 @@ import sys
 import subprocess
 import getopt
 
-def split_live(duration = "00:01:00.00", numClips = 1, ffmpeg = None):
-    argv = sys.argv[1:]
+default_url = "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1701512187/ei/m69qZfDiHeKX2_gPnreS6A8/ip/64.229.43.235/id/jfKfPfyJRdk.2/itag/96/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D137/rqh/1/hls_chunk_host/rr2---sn-cxaaj5o5q5-t34l.googlevideo.com/xpc/EgVo2aDSNQ%3D%3D/playlist_duration/30/manifest_duration/30/spc/UWF9f7Pyl95o9A3VLizSSUApyGTWt6Cj_wQmPSZlUQ/vprv/1/playlist_type/DVR/mh/rr/mm/44/mn/sn-cxaaj5o5q5-t34l/ms/lva/mv/u/mvi/2/pl/27/dover/11/pacing/0/keepalive/yes/fexp/24007246/mt/1701489735/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,rqh,xpc,playlist_duration,manifest_duration,spc,vprv,playlist_type/sig/ANLwegAwRAIgIgsZq9-Cv5tY_vKt7Gt_y_fnemrznfbWxphmURn0R6ACIG5xC572UwrPpLKyyFJzbByYTDNRsShVykBo4ENZq1Fj/lsparams/hls_chunk_host,mh,mm,mn,ms,mv,mvi,pl/lsig/AM8Gb2swRgIhAMJ00ThGc3YSY-gGrN3-VdaANTN-TKuiXRfDoX3WjPhQAiEAso97lm9JcsJgj1bKdiIbR582UWXuKbczCExGdn6Zpf0%3D/playlist/index.m3u8"
 
-    #for test runs
-    if argv != None:
-        url = argv[0]
-        duration = argv[1]
-        numClips = argv[2]
+argv = sys.argv[1:]
 
-        #set ffmpeg location if needed
-        opt, arg = getopt.getopt(argv, "f", "ffmpeg=")
+#for test runs
+if argv != None:
+    url = argv[0]
+    duration = argv[1]
+    numClips = argv[2]
+
+    #set ffmpeg location if needed
+    opt, arg = getopt.getopt(argv, "f", "ffmpeg=")
 
     for name, value in opt:
         if name in ['-f', '--ffmpeg']:
             ffmpeg = value
+
+def split_live(duration = "00:01:00.00", numClips = 1, ffmpeg = None, url = default_url):
 
     commands = [ffmpeg, '-i', url, '-t', duration, '-c', "copy", "clip.mp4"]
 
@@ -39,3 +42,4 @@ def split_live(duration = "00:01:00.00", numClips = 1, ffmpeg = None):
     
     print("Livestream clips downloaded")
 
+split_live(duration, numClips, ffmpeg, url)
